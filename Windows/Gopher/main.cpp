53ed33700b221aae7cddffbb5cb0f4d2df5aaa3b
witsar <fabian.sanz@posteo.de>
2023-03-20 17:34:04 +0100

    Allow input from up to 4 devices

--- Windows/Gopher/main.cpp
+++ Windows/Gopher/main.cpp
@@ -22,6 +22,7 @@
 
 #include <windows.h> // for Beep()
 #include <iostream>
+#include <thread>
 
 
 #pragma comment(lib, "XInput9_1_0.lib")
@@ -40,24 +41,31 @@ BOOL isRunningAsAdministrator(); // Check if administrator, makes on-screen keyb
  *   http://msdn.microsoft.com/en-us/library/windows/desktop/microsoft.directx_sdk.reference.xinput_gamepad%28v=vs.85%29.aspx
  */
 
+void programLoop(Gopher gopher)
+{
+    while (true)
+    {
+        gopher.loop();
+    }
+}
+
 int main()
 {
-  CXBOXController controller(1);
-  Gopher gopher(&controller);
+  CXBOXController controllers[] = {1,2,3,4};
+  Gopher gopherP1(&controllers[0]);
+  Gopher gopherP2(&controllers[1]);
+  Gopher gopherP3(&controllers[2]);
+  Gopher gopherP4(&controllers[3]);
   HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
   SetConsoleTitle( TEXT( "Gopher360" ) );
 
-  system("Color 1D");
-
-  printf("Welcome to Gopher360 - a VERY fast and lightweight controller-to-keyboard & mouse input tool.\n");
-  printf("All you need is an Xbox360/Xbone controller (wired or wireless adapter), or DualShock (with InputMapper 1.5+)\n");
+  printf("Welcome to Gopher360 Multiplayer Edition - a VERY fast and lightweight controller-to-keyboard & mouse input tool.\n");
+  printf("All you need is 1-4 Xbox360/Xbone controller(s) (wired or wireless adapter), or DualShock (with InputMapper 1.5+)\n");
   printf("Gopher will autofind the xinput device and begin reading input - if nothing happens, verify connectivity.\n");
   printf("See the GitHub repository at bit.ly/1syAhMT for more info. Twitter contact: TylerAt60FPS\n\n-------------------------\n\n");
-  
-  SetConsoleTextAttribute(hConsole, 23);
+
   printf("Gopher is free (as in freedom) software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n");
   printf("\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see http://www.gnu.org/licenses/.");
-  SetConsoleTextAttribute(hConsole, 29);
   printf("\n\n-------------------------\n\n");
 
   SetConsoleTextAttribute(hConsole, 5); // set color to purple on black (windows only)
@@ -71,13 +79,20 @@ int main()
     printf("Tip - Not running as an admin! Windows on-screen keyboard and others won't work without admin rights.\n");
   }
 
-  gopher.loadConfigFile();
+  gopherP1.loadConfigFile();
+  gopherP2.loadConfigFile();
+  gopherP3.loadConfigFile();
+  gopherP4.loadConfigFile();
 
-  // Start the Gopher program loop
-  while (true)
-  {
-    gopher.loop();
-  }
+  // Start the Gopher program loops
+  std::thread programLoopP1(programLoop, gopherP1);
+  std::thread programLoopP2(programLoop, gopherP2);
+  std::thread programLoopP3(programLoop, gopherP3);
+  std::thread programLoopP4(programLoop, gopherP4);
+  programLoopP1.join();
+  programLoopP2.join();
+  programLoopP3.join();
+  programLoopP4.join();
 }
 
 BOOL isRunningAsAdministrator()
